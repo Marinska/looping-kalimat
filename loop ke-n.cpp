711e707a84bb0ba2540669adde6cc31b77dab56d
Marinska <Marinska354@users.noreply.github.com>
2018-12-17 14:07:44 +0700

    source code

---
+++ loop ke-n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; //Nama : Umar Ibnu Zainal Muttaqin
+void logika (int n) //NIM : 311810909
+{
+	for (int i=1;i<=n;i=i+1)
+	{
+		cout<<"Algoritma dan Pemrograman\n";
+	}
+}
+
+int main()
+{
+	int a;
+	cout<<"Masukan nilai pengulangan : ";
+	cin>>a;
+	logika(a);
+}
